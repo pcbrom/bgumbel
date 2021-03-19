b14303bda86b294b59a9fff244226e8c451fe9bb
Pedro Carvalho Brom <pcbrom@gmail.com>
2021-03-19 13:04:05 -0300

    noDL again

--- R/about.R
+++ R/about.R
@@ -1,4 +1,5 @@
 .onAttach <- function(libname, pkgname) {
+  if (!capabilities('long.double')) {options(matprod = 'internal')}
   # packageStartupMessage({
   #   "Dependencies for rbgumbel function: MCMCpack, coda, MASS."
   # })
--- R/dbgumbel.R
+++ R/dbgumbel.R
@@ -19,3 +19,6 @@ dbgumbel <- function(x, mu, sigma, delta) {
 }
 
 dbgumbel <- Vectorize(dbgumbel, 'x')
+
+
+
